e1dfb993da1c3818af7d22c267f475b8f0fba675
Admin Rafał Jakonis <admin_rj@kornice.ekookna>
2020-07-24 13:05:55 +0200

    Initial comit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Post;
+use AppBundle\Form\Formularz;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('default/base.html.twig', [
+        ]);
+    }
+    /**
+     * @Route("/formularz", name="form")
+     */
+    public function formAction(Request $request)
+    {
+        $post = new Post;
+        $form = $this->createForm(Formularz::class, $post);
+        $form->handleRequest($request);
+        if($form->isSubmitted())
+        {
+            $plik = $form->get('file')->getData();
+            if($plik)
+            {
+            $unikatowa= uniqid().'.'.$plik->guessExtension();
+            $plik->move($this->getParameter('pliki'),$unikatowa);
+            }
+            else
+            {
+                $unikatowa = " ";
+            }
+            $post ->setFile($unikatowa);
+            $post ->setVision(0);
+            $post = $form->getData();
+            $eM = $this->getDoctrine()->getManager();
+            $eM->persist($post);
+            $eM->flush();
+            
+            
+            
+            
+        }
+        return $this->render('default/index.html.twig', ['formularz' => $form->createView()]);
+    }
+        /**
+     * @Route("/panel_administacyjny", name="panel")
+     */
+    public function controlAction()
+    {
+        $eM = $this->getDoctrine()->getManager();
+        $produkty = $eM ->getRepository('AppBundle\Entity\Post')->findAll();       
+        return $this->render('default/control.html.twig',['wyniki'=>$produkty ]);
+    }
+    /**
+     * @Route("/panel_administacyjny/zgłosznie/{id}", name="panelShowNote")
+     */
+    public function controlShowNoteAction($id)
+    {     
+        
+        $eM = $this->getDoctrine()->getManager();
+        $produkt= $eM->getRepository('AppBundle\Entity\Post')->find($id);
+        $produkt->setVision(1);
+        $eM->flush();
+        return $this->render('default/note.html.twig',['zgloszenia'=>$produkt]);;            
+    }
+    /**
+     * @Route("/panel_administacyjny/usuwanie/{id}", name="panelDel")
+     */
+    public function controlDelNoteAction($id)
+    {
+        $eM = $this->getDoctrine()->getManager();
+        $produkt = $eM ->getRepository('AppBundle\Entity\Post')->find($id);   
+        $eM -> remove($produkt);
+        $eM -> flush();
+        $produkty =$eM ->getRepository('AppBundle\Entity\Post')->findAll();
+        return $this->render('default/control.html.twig',['wyniki'=>$produkty]);
+    }
+
+
+
+}
---
+++ src/AppBundle/Form/Formularz.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\File;
+class Formularz extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        
+        $builder
+            ->add('title',ChoiceType::class,[
+                'choices'  => [
+                    'Zmiana przystanku' => "Zmiana przystanku",
+                    'Zmiana godziny odjazdu autobusu' => "Godzina odjazdu",
+                    'Zmiana godziny przyjazdu odjazdu' => "Godzina",
+            ]])
+            ->add('content',TextareaType::class,array('attr' =>array('class'=>'tresc')))
+            ->add('file', FileType::class,['required' => false,'constraints'=> [new File(['maxSize'=>'1M','maxSizeMessage'=>'plik jest za duży'])]]);
+            
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+    }
+}
